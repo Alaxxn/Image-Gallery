31688e8dd612614e050fe5d22e8d13a9081cbf9b
Alan Castillo <acast125@calpoly.edu>
2025-06-06 23:32:13 -0700

    image searching

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,26 +7,26 @@ import { Routes, Route } from "react-router";
 import { useState, useEffect } from "react";
 import { ValidRoutes } from "csc437-monorepo-backend/src/shared/ValidRoutes.ts";
 import type { IApiImageData } from "csc437-monorepo-backend/src/shared/ApiImageData.ts";
+import {ImageSearchForm} from "./images/ImageSearchForm.tsx";
 
 
 function App() {
     const [imageData, _setImageData] = useState<IApiImageData[]>([]);
     const [loading, _setLoading] = useState(true);
+    const [searchTerm, _setSearchTerm] = useState("");
     const [error, _setError] = useState(false);
 
-  useEffect(() => {
     const fetchImages = async () => {
       try {
-        const response = await fetch("/api/images");
+        const endpoint = `http://localhost:3000/api/images/search?name=${searchTerm}`
+        const response = await fetch(endpoint);
         if (!response.ok) {
           console.log("Error status:", response.status);
           _setError(true);
           _setLoading(false);
           return;
         }
-
         const data = await response.json();
-        console.log(data);
         _setImageData(data);
         _setLoading(false);
       } catch (err) {
@@ -36,13 +36,29 @@ function App() {
       }
     };
 
+  useEffect(() => {
     fetchImages();
   }, []);
+
+  function handleImageSearch() {
+    fetchImages();
+  }
     
-    return (
+  return (
     <Routes>
       <Route path={ValidRoutes.HOME} element={<MainLayout />} >
-        <Route index element={<AllImages data={imageData} loading={loading} error={error}/>} />
+        <Route index element={
+          <AllImages 
+            data={imageData} 
+            loading={loading} 
+            error={error} 
+            searchPanel= {<ImageSearchForm 
+                  searchString = {searchTerm}
+                  onSearchStringChange = {_setSearchTerm}
+                  onSearchRequested = {handleImageSearch}
+            
+              />} 
+          />}/>
         <Route path={ValidRoutes.IMAGES} 
         element={<ImageDetails data={imageData} loading={loading} error={error} changeData={_setImageData}/>}/>
         <Route path={ValidRoutes.LOGIN} element={<LoginPage />} />
--- packages/frontend/src/images/AllImages.tsx
+++ packages/frontend/src/images/AllImages.tsx
@@ -4,7 +4,8 @@ import { ImageGrid } from "./ImageGrid.tsx";
 interface ImageProp {
     data: IApiImageData[];
     loading : boolean;
-    error : boolean;
+    error : boolean
+    searchPanel : React.ReactNode;
 }
 
 export function AllImages(props : ImageProp) {
@@ -12,8 +13,11 @@ export function AllImages(props : ImageProp) {
     if (props.loading) return <p>Loading images...</p>;
     if (props.error) return <p>Failed to load Image. Please try again later.</p>;
 
+    const searchPanel = props.searchPanel;
+
     return (
         <div className="container">
+            {searchPanel}
             <h2>All Images</h2>
             <ImageGrid images={props.data} />
         </div>
